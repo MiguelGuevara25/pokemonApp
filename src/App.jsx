28fb6e77c75898b481c37e8201dafba4e5da8fe9
MiguelGuevara25 <miguel_g1789@hotmail.com>
2023-05-18 13:08:33 -0500

    feat: :sparkles: DEV Testing the API connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const consult = async () => {
+      const url = `https://pokeapi.co/api/v2/pokemon`;
+      const response = await fetch(url);
+      const data = await response.json();
+
+      console.log(data);
+    };
+
+    consult();
+  }, []);
+
   return (
     <>
       <h1>Fight Simulator Pokemon</h1>
