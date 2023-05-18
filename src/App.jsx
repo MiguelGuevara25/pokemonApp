f1b93eb5db30a01ec528bd6cd60251e033417c2a
MiguelGuevara25 <miguel_g1789@hotmail.com>
2023-05-18 14:09:58 -0500

    wip: :construction: :construction_worker: DEV Displaying the data on the screen, but it is not finished yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,40 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemon2, setPokemon2] = useState({});
+
   useEffect(() => {
     const consult = async () => {
       const url = `https://pokeapi.co/api/v2/pokemon`;
       const response = await fetch(url);
       const data = await response.json();
 
-      console.log(data);
+      const url2 = `https://pokeapi.co/api/v2/pokemon/${data.results[4].name}`;
+      const response2 = await fetch(url2);
+      const data2 = await response2.json();
+
+      setPokemon(data.results);
+      setPokemon2(data2);
     };
 
     consult();
   }, []);
 
+  const { name, sprites } = pokemon2;
+
   return (
     <>
       <h1>Fight Simulator Pokemon</h1>
+      {pokemon.map((poke, id) => (
+        <div key={id + 1}>
+          <h1 className="capitalize">{poke.name}</h1>
+        </div>
+      ))}
+
+      <h1>OTRO POKEMON</h1>
+      <p className="capitalize">{name}</p>
+      <img src={sprites.front_default} alt={name} />
     </>
   );
 }
