624a16f7e846316d3f2802f732730b3ecfbeef3e
MiguelGuevara25 <miguel_g1789@hotmail.com>
2023-05-31 19:18:14 -0500

    wip: :construction: DEV Added buttons and pokemon power, but no reduction in the life of any pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   const [modalFightPokemon, setModalFightPokemon] = useState(false);
 
   const getAllPokemons = async () => {
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=50&offset=0`;
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=150&offset=0`;
     const response = await fetch(url);
     const data = await response.json();
 
--- src/components/ScreenFIght.jsx
+++ src/components/ScreenFIght.jsx
@@ -1,37 +1,49 @@
-import { Typography, CardMedia } from "@mui/material";
+import { Typography, Button, CardMedia } from "@mui/material";
+import { useState } from "react";
 
-const ScreenFIght = ({ pokemones, setModalFightPokemon }) => {
+const ScreenFIght = ({ pokemones }) => {
   return (
     <div>
       <div className="fixed inset-x-0 bottom-0 top-0 z-[1000] flex w-full items-center justify-center bg-black">
         <div className="bg-white w-11/12 h-[95%] flex justify-center items-center">
-          {pokemones.map((poke, index) => (
-            <div key={index}>
-              <CardMedia
-                sx={{
-                  height: 200,
-                  width: 200,
-                  transform: index === 0 && "scaleX(-1)",
-                }}
-                image={poke.sprites.front_default}
-              />
+          {pokemones.map((poke, index) => {
+            return (
+              <div key={index} className="flex flex-col items-center">
+                <CardMedia
+                  sx={{
+                    height: 200,
+                    width: 200,
+                    transform: index === 0 && "scaleX(-1)",
+                  }}
+                  image={poke.sprites.front_default}
+                />
 
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                className="capitalize"
-              >
-                {poke.name}
-              </Typography>
-            </div>
-          ))}
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                  className="capitalize"
+                >
+                  {poke.name}
+                </Typography>
+
+                <progress
+                  id="file"
+                  max={poke.stats[0].base_stat}
+                  value={poke.stats[0].base_stat}
+                ></progress>
+                <Button variant="contained">
+                  {poke.abilities[0].ability.name}
+                </Button>
+                <Button variant="contained">
+                  {poke.abilities[1].ability.name}
+                </Button>
+                <p>{poke.stats[1].base_stat}</p>
+                <p>{poke.stats[2].base_stat}</p>
+              </div>
+            );
+          })}
         </div>
-
-        <div
-          className="absolute inset-0"
-          onClick={() => setModalFightPokemon(false)}
-        />
       </div>
     </div>
   );
